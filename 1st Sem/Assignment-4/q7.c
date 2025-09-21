58e9b3d1f9a6d6d628dd6cfc6c1b18014a410247
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 10:48:52 +0530

    Create q7.c

---
+++ 1st Sem/Assignment-4/q7.c
@@ -0,0 +1,18 @@
+/*
+Q7) Check whether an entered number is even or odd using switch-case statement.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    switch(num % 2) {
+        case 0: printf("Even\n"); break;
+        case 1: printf("Odd\n"); break;
+    }
+
+    return 0;
+}
