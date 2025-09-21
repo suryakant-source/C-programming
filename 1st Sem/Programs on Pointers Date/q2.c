989867e29d3d91c03a07cb156844682b40456183
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 14:27:40 +0530

    Create q2.c

---
+++ 1st Sem/Programs on Pointers Date/q2.c
@@ -0,0 +1,24 @@
+/*
+Q2) Create a user defined function to find sum of all array elements using pointer
+*/
+
+#include <stdio.h>
+
+int sumArray(int *arr, int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) sum += *(arr + i);
+    return sum;
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    printf("Sum of array elements: %d\n", sumArray(arr, n));
+    return 0;
+}
