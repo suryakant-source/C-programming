6f66751347230205dc99011c748f5754900961e2
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 14:27:56 +0530

    Create q3.c

---
+++ 1st Sem/Programs on Pointers Date/q3.c
@@ -0,0 +1,23 @@
+/*
+Q3) Create a user defined function to swap the values of two variables using call by reference
+*/
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
