d96f6eea487fb271b4e455f082d3188e11cee701
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 11:43:41 +0530

    Create q8.c

---
+++ 1st Sem/Programs on Array/q8.c
@@ -0,0 +1,21 @@
+/*
+Q8) Reverse the elements of the array
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    printf("Reversed array: ");
+    for(int i = n - 1; i >= 0; i--) printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
