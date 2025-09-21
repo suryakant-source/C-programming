3e05e3b499757a4fa01aaaf77bf8d412f0964e93
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 10:26:34 +0530

    Create q14.c

---
+++ 1st Sem/Assignment 1/q14.c
@@ -0,0 +1,16 @@
+/*
+Q14) Swap the values of two variables without using a third variable.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
