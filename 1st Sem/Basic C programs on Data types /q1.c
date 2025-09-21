5f5ea5442d557898ce6b479b571b55ff35438aa1
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 11:39:58 +0530

    update

---
+++ 1st Sem/Basic C programs on Data types /q1.c
@@ -0,0 +1,12 @@
+/*
+Q1) Print your Name, SIC Number and Section at separate lines.
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Name: Suryakant Sahoo\n");
+    printf("SIC Number: 123456\n");
+    printf("Section: c\n");
+    return 0;
+}
