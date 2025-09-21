9b62df74f6d7a284adc3de3038818cb8f45ee35f
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 10:22:10 +0530

    Create q5.c

---
+++ 1st Sem/Assignment 1/q5.c
@@ -0,0 +1,15 @@
+/*
+Q5) Print your details in the below format:
+* * * Name: Suryakant sahoo * * *
+* * * Branch: CSE * * *
+* * * Roll No: 5* * *
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("* * * Name: Suryakant Sahoo * * *\n");
+    printf("* * * Branch: CSE * * *\n");
+    printf("* * * Roll No: 5 * * *\n");
+    return 0;
+}
