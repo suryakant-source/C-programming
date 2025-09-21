59b560e25b5f123f53f794f80e201b89398d4647
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 10:52:36 +0530

    Create q6.c

---
+++ 1st Sem/Assignment-5/q6.c
@@ -0,0 +1,20 @@
+/*
+Q6) Find the factorial of any user entered number using for loop.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    printf("Factorial of %d = %llu\n", n, fact);
+    return 0;
+}
