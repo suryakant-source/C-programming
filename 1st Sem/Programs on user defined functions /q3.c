e2e1f9bb051efed761daed07eaed538c43fd29c6
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 12:09:56 +0530

    Create q3.c

---
+++ 1st Sem/Programs on user defined functions /q3.c
@@ -0,0 +1,20 @@
+/*
+Q3) Design a function to find the factorial of a number
+*/
+
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++) fact *= i;
+    return fact;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d is %d\n", num, factorial(num));
+    return 0;
+}
