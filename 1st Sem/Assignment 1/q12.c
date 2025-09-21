19bdc563b9748b7804b2771ba0cdbcb257e3fca6
suryakant-source <xbsuryakant@gmail.com>
2025-09-21 10:25:52 +0530

    Create q12.c

---
+++ 1st Sem/Assignment 1/q12.c
@@ -0,0 +1,14 @@
+/*
+Q12) Calculate simple interest.
+*/
+
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, si;
+    printf("Enter principal, rate, and time: ");
+    scanf("%f %f %f", &principal, &rate, &time);
+    si = (principal * rate * time) / 100;
+    printf("Simple Interest = %.2f\n", si);
+    return 0;
+}
